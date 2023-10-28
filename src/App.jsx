7777d3d122888a7afd5aa241fecb09ac5af23280
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-10-28 22:53:45 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Hello</h1>
+        <p>Paragraph</p>
+      </div>
+    </>
+  )
+}
+
+const MainContainer = styled.div`
+display: flex;
+align-items: center;
+`
+export default App
